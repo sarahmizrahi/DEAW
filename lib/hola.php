fbb251505a91c490e1d5b98204e28a4758eebf40
sarahmizrahi <145108904+sarahmizrahi@users.noreply.github.com>
2024-05-10 10:58:25 +0200

    se ha colado /lib
    eliminado

--- lib/hola.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Autor: Sarah Mizrahi Roig <sarmizroi@alu.edu.gva.es>
-require('HolaMundo.php');
-
-print "Introduce tu nombre:";
-$nombre = trim(fgets(STDIN));
-print new HolaMundo($nombre);
-?>
